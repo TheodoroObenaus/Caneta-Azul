fb07398bfcf043e3b8d95f6c2eac445a24aa9859
TheodoroObenaus <theodoro.obenaus@gmail.com>
2023-04-10 11:49:32 -0300

    Barigadam

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,20 @@ const nome = ref('')
 const email = ref('')
 const senha = ref('')
 const senha1 = ref('')
+const data = ref('')
+const endereço = ref('')
+const cidade = ref('')
+const estado = ref('')
+const hobie = ref('')
+const lingua = ref('')
+const biografia = ref('')
+
 </script>
 
 <template>
 
-  <div>
+<form>
+  <div class="corpo">
     <p>Nome: </p>
     <input type="text" v-model="nome" placeholder="Digite seu nome" />
     <p>Email: </p>
@@ -18,16 +27,76 @@ const senha1 = ref('')
     <input type="password" v-model="senha" placeholder="Digite sua senha" />
     <p>Confirmar senha: </p>
     <input type="password" v-model="senha1" placeholder="Digite sua senha" />
-    <div v-if="senha = senha1">
+    <div>
       ta certa a senha
     </div>
-    <div v-else>
+    <div>
       a senha ta errada
     </div>
+    <p>Data de Nascimento;</p>
+    <input type="date" v-model="data">
+    <p>Endereço:</p>
+    <input type="text" v-model="endereço" placeholder="rua;Nºda casa;">
+    <p>Cidade:</p>
+    <input type="text" v-model="cidade" placeholder="Digite seu Endereço">
+    <p>Estado:</p>
+    <select v-model="estado">
+      <option value="AC">Acre</option>
+      <option value="AL">Alagoas</option>
+      <option value="AP">Amapá</option>
+      <option value="AM">Amazonas</option>
+      <option value="BA">Bahia</option>
+      <option value="CE">Ceará</option>
+      <option value="DF">Distrito Federal</option>
+      <option value="ES">Espírito Santo</option>
+      <option value="GO">Goiás</option>
+      <option value="MA">Maranhão</option>
+      <option value="MT">Mato Grosso</option>
+      <option value="MS">Mato Grosso do Sul</option>
+      <option value="MG">Minas Gerais</option>
+      <option value="PA">Pará</option>
+      <option value="PB">Paraíba</option>
+      <option value="PR">Paraná</option>
+      <option value="PE">Pernambuco</option>
+      <option value="PI">Piauí</option>
+      <option value="RJ">Rio de Janeiro</option>
+      <option value="RN">Rio Grande do Norte</option>
+      <option value="RS">Rio Grande do Sul</option>
+      <option value="RO">Rondônia</option>
+      <option value="RR">Roraima</option>
+      <option value="SC">Santa Catarina</option>
+      <option value="SP">São Paulo</option>
+      <option value="SE">Sergipe</option>
+      <option value="TO">Tocantins</option>
+    </select>
+    <p>Hobbies:</p>
+    <input type="text" v-model="hobie" placeholder="Digite seus hobbies">
+    <p>Linguagem que você programa:</p>
+    <select v-model="lingua">
+      <option value="JavaScript">JavaScript</option>
+      <option value="C++">C++</option>
+      <option value="Python">Python</option>
+      <option value="Java">Java</option>
+      <option value="C#">C#</option>
+    </select>
+    <p>Biografia:</p>
+    <textarea v-model="biografia" cols="30" rows="10"></textarea>
   </div>
+</form> 
 
 </template>
 
 <style scoped>
+
+form {
+  grid-template-columns: 50% 50%;
+  margin: 25px 25px 25px 25px;
+}
+.corpo {
+  border: 5px solid red;
+  border-radius: 50px;
+  text-align: center;
+  padding: 25px;
+}
 
 </style>
